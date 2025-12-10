abf8dbe6cdeec3f2dc67b700e6af46df9abf3f27
balu-bunny <balakrishna@qualityze.com>
2025-12-10 17:52:02 +0530

    removed unwanted imports

--- src/components/OrgCreate.tsx
+++ src/components/OrgCreate.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import OrgModal from "./OrgModal";
 import { createOrg as apiCreateOrg } from "../api/dashboard";
 
--- src/components/OrgList.tsx
+++ src/components/OrgList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { subscribeToOrgs } from "../api/dashboard";
 
